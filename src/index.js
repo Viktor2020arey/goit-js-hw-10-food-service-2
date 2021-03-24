8b610b46c644dced5d6ef5d9732ae6bd855e6929
Holovatiuk Victor <Stethem696@gmail.com>
2021-03-25 01:06:52 +0200

    Создана динамическая разметка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import menuElm from './menu.json';
+import menuTml from './templates/templates.hbs';
+
 import './css/styles.css';
+
+const galleryEl = document.querySelector('.js-menu');
+
+const marcup = menuTml(menuElm);
+
+galleryEl.insertAdjacentHTML('beforeend', marcup);
